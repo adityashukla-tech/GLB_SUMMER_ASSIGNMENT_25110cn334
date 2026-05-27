fc4cfd3cfa7b309e899352ba2479b54c425ad5b9
ADITYA SHUKLA <adityashuklaknp07@gmail.com>
2026-05-27 17:57:55 +0530

    Add files via upload

---
+++ Q2_Multiplication_Table.java
@@ -0,0 +1,15 @@
+package summer_assignments;
+import java.util.*;
+public class Q2_Multiplication_Table {
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int num,i,multi=0;
+    System.out.print("Enter the Numebr: ");
+    num=sc.nextInt();
+    for(i=1;i<=10;i++){
+        multi=num*i;
+        System.out.println(num+" x "+i+" = "+multi);
+    }
+}
+}
+
