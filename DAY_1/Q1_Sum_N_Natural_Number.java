3b2effd17e19a4a8266d94a0bdc55b4000920f78
ADITYA SHUKLA <adityashuklaknp07@gmail.com>
2026-05-28 14:43:31 +0530

    Add files via upload

---
+++ DAY_1/Q1_Sum_N_Natural_Number.java
@@ -0,0 +1,16 @@
+package summer_assignments;
+import java.util.*;
+import java.util.Scanner;
+public class Q1_Sum_N_Natural_Number {
+    public static void main(String[] args){
+    int num,sum=0,i;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the Number til Summing: ");
+    num=sc.nextInt();
+    for( i=1;i<=num;i++) {
+        sum+=i;
+    }
+    System.out.println("Answer: "+sum);
+    sc.close();
+    }
+}
