3e6c70715b87b5b9ec36176cd344b43c191f69db
ADITYA SHUKLA <adityashuklaknp07@gmail.com>
2026-05-28 14:42:57 +0530

    Add files via upload

---
+++ DAY_3/Q12_LCM.java
@@ -0,0 +1,27 @@
+package summer_assignments;
+import java.util.*;
+public class Q12_LCM {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num1,num2,a,b;
+        System.out.print("Enter the Number A: ");
+        num1=sc.nextInt();
+        System.out.print("Enter teh Number B: ");
+        num2=sc.nextInt();
+        a=Math.abs(num1);
+        b=Math.abs(num2);
+        if(a==0 || b==0){
+            System.out.print("LCM = "+0);
+            sc.close();
+            return;
+        }
+        while(b>0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        int lcm=Math.abs(num1)*(Math.abs(num2)/a);
+        System.out.print("LCM = "+lcm);
+        sc.close();
+    }
+}
---
+++ DAY_3/Q9_Prime_Number.java
@@ -0,0 +1,27 @@
+package summer_assignments;
+import java.util.*;
+public class Q9_Prime_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num, i, flag = 0;
+        System.out.print("Enter the Number: ");
+        num = sc.nextInt();
+        if(num<=1){
+            System.out.print("Entered Number is NOT Prime");
+            sc.close();
+            return;
+        }
+        for (i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1) {
+            System.out.print("Entered Number is NOT prime");
+        } else {
+            System.out.print("Entered Number IS Prime");
+        }
+        sc.close();
+    }
+}
